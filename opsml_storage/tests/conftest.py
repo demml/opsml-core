75c3c509326a66cb9657b3ec3db723e4d34c56d3
Thorrester <sjforrester32@gmail.com>
2024-11-12 14:11:19 -0500

    Update conftest.py

--- opsml_storage/tests/conftest.py
+++ opsml_storage/tests/conftest.py
@@ -7,4 +7,5 @@ from opsml_storage import GCSFSStorageClient
 def gcs_storage_client():
     value = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
     assert value, "GOOGLE_APPLICATION_CREDENTIALS not set"
-    return GCSFSStorageClient(bucket=os.environ["GCS_BUCKET_NAME"])
+    print(f"GOOGLE_APPLICATION_CREDENTIALS: {value}")
+    return GCSFSStorageClient(bucket=os.environ["GCP_BUCKET_NAME"])
