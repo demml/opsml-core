bc82bb368a1fda89f09b3c0ac30d1207652ba784
Thorrester <sjforrester32@gmail.com>
2024-11-27 11:57:43 -0500

    refactor bash

--- crates/test.sh
+++ crates/test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-export OPSML_STORAGE_URI=${OPSML_STORAGE_URI}
+# Set default values if environment variables are not set
+: "${OPSML_STORAGE_URI:=default_storage_uri}"
+: "${OPSML_SERVER_PORT:=3000}"
+: "${TEST_NAME:=test_filesystemstorage_with_http_google}"
+
+export OPSML_STORAGE_URI
+export PORT=$OPSML_SERVER_PORT
 
 # Start the opsml_server in the background
 cargo run opsml_server &
@@ -16,8 +22,7 @@ trap "kill $SERVER_PID" EXIT
 # Wait for the server to start (adjust the sleep time as needed)
 sleep 5
 
-export OPSML_TRACKING_URI="http://localhost:3000"
+export OPSML_TRACKING_URI="http://localhost:$PORT"
 
 # Run the tests
-TEST_NAME=${TEST_NAME:-test_filesystemstorage_with_http_google}
 cargo test -p opsml-storage $TEST_NAME -- --nocapture
