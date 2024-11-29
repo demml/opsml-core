db2b65debd5676288cac5e54b837949138aae42a
Thorrester <sjforrester32@gmail.com>
2024-11-29 09:30:32 -0500

    adding migrations

--- crates/opsml_sql/src/base.rs
+++ crates/opsml_sql/src/base.rs
@@ -1,1 +1,7 @@
+use async_trait::async_trait;
+use opsml_settings::config::OpsmlStorageSettings;
 
+#[async_trait]
+pub trait FileSystem {
+    async fn new(settings: &OpsmlStorageSettings) -> Self;
+}
---
+++ crates/opsml_sql/src/sqlite/migrations/20241129142517_setup_tables.sql
@@ -0,0 +1,132 @@
+-- Add migration script here
+-- SQLite Migration Script
+
+-- DataSchema
+CREATE TABLE IF NOT EXISTS opsml_data_registry (
+    uid TEXT PRIMARY KEY,
+    date TEXT,
+    timestamp INTEGER,
+    app_env TEXT DEFAULT 'development',
+    name TEXT,
+    repository TEXT,
+    version TEXT NOT NULL,
+    contact TEXT,
+    tags TEXT,
+    data_type TEXT,
+    runcard_uid TEXT,
+    pipelinecard_uid TEXT,
+    auditcard_uid TEXT,
+    interface_type TEXT NOT NULL DEFAULT 'undefined'
+);
+
+-- ModelSchema
+CREATE TABLE IF NOT EXISTS opsml_model_registry (
+    uid TEXT PRIMARY KEY,
+    date TEXT,
+    timestamp INTEGER,
+    app_env TEXT DEFAULT 'development',
+    name TEXT,
+    repository TEXT,
+    version TEXT NOT NULL,
+    contact TEXT,
+    tags TEXT,
+    datacard_uid TEXT,
+    sample_data_type TEXT,
+    model_type TEXT,
+    runcard_uid TEXT,
+    pipelinecard_uid TEXT,
+    auditcard_uid TEXT,
+    interface_type TEXT NOT NULL DEFAULT 'undefined',
+    task_type TEXT NOT NULL DEFAULT 'undefined'
+);
+
+-- RunSchema
+CREATE TABLE IF NOT EXISTS opsml_run_registry (
+    uid TEXT PRIMARY KEY,
+    date TEXT,
+    timestamp INTEGER,
+    app_env TEXT DEFAULT 'development',
+    name TEXT,
+    repository TEXT,
+    version TEXT NOT NULL,
+    contact TEXT,
+    tags TEXT,
+    datacard_uids TEXT,
+    modelcard_uids TEXT,
+    pipelinecard_uid TEXT,
+    project TEXT,
+    artifact_uris TEXT,
+    compute_environment TEXT
+);
+
+-- AuditSchema
+CREATE TABLE IF NOT EXISTS opsml_audit_registry (
+    uid TEXT PRIMARY KEY,
+    date TEXT,
+    timestamp INTEGER,
+    app_env TEXT DEFAULT 'development',
+    name TEXT,
+    repository TEXT,
+    version TEXT NOT NULL,
+    contact TEXT,
+    tags TEXT,
+    approved BOOLEAN,
+    datacards TEXT,
+    modelcards TEXT,
+    runcards TEXT
+);
+
+-- PipelineSchema
+CREATE TABLE IF NOT EXISTS opsml_pipeline_registry (
+    uid TEXT PRIMARY KEY,
+    date TEXT,
+    timestamp INTEGER,
+    app_env TEXT DEFAULT 'development',
+    name TEXT,
+    repository TEXT,
+    version TEXT NOT NULL,
+    contact TEXT,
+    tags TEXT,
+    pipeline_code_uri TEXT,
+    datacard_uids TEXT,
+    modelcard_uids TEXT,
+    runcard_uids TEXT
+);
+
+-- ProjectSchema
+CREATE TABLE IF NOT EXISTS opsml_project_registry (
+    uid TEXT,
+    name TEXT,
+    repository TEXT,
+    project_id INTEGER PRIMARY KEY,
+    version TEXT NOT NULL,
+    timestamp INTEGER
+);
+
+-- MetricSchema
+CREATE TABLE IF NOT EXISTS opsml_run_metrics (
+    run_uid TEXT,
+    name TEXT,
+    value REAL,
+    step INTEGER,
+    timestamp INTEGER,
+    date_ts TEXT DEFAULT (datetime('now')),
+    idx INTEGER PRIMARY KEY
+);
+
+-- ParameterSchema
+CREATE TABLE IF NOT EXISTS opsml_run_parameters (
+    run_uid TEXT,
+    name TEXT,
+    value TEXT,
+    date_ts TEXT DEFAULT (datetime('now')),
+    idx INTEGER PRIMARY KEY
+);
+
+-- HardwareMetricSchema
+CREATE TABLE IF NOT EXISTS opsml_run_hardware_metrics (
+    run_uid TEXT NOT NULL,
+    created_at TEXT DEFAULT (datetime('now')),
+    metrics TEXT,
+    idx INTEGER PRIMARY KEY
+);
