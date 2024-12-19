ab19dcf97327937e6b1dfd91cb4497dacbbd8ae7
Thorrester <sjforrester32@gmail.com>
2024-12-19 14:12:30 -0500

    xgboost interface args

--- crates/opsml_types/src/cards/model/base.rs
+++ crates/opsml_types/src/cards/model/base.rs
@@ -489,3 +489,51 @@ impl VowpalWabbitInterfaceArgs {
         }
     }
 }
+
+#[pyclass]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct XGBoostModelInterfaceArgs {
+    #[pyo3(get)]
+    pub task_type: String,
+    #[pyo3(get)]
+    pub model_type: String,
+    #[pyo3(get)]
+    pub data_type: String,
+    #[pyo3(get)]
+    pub modelcard_uid: String,
+    #[pyo3(get)]
+    pub feature_map: HashMap<String, Feature>,
+    #[pyo3(get)]
+    pub sample_data_interface_type: String,
+    #[pyo3(get)]
+    pub preprocessor_name: String,
+    #[pyo3(get)]
+    pub metadata: HashMap<String, String>,
+}
+
+#[pymethods]
+impl XGBoostModelInterfaceArgs {
+    #[new]
+    #[pyo3(signature = (task_type, model_type, data_type, modelcard_uid, feature_map, sample_data_interface_type, preprocessor_name, metadata=None))]
+    fn new(
+        task_type: String,
+        model_type: String,
+        data_type: String,
+        modelcard_uid: String,
+        feature_map: HashMap<String, Feature>,
+        sample_data_interface_type: String,
+        preprocessor_name: String,
+        metadata: Option<HashMap<String, String>>,
+    ) -> Self {
+        XGBoostModelInterfaceArgs {
+            task_type,
+            model_type,
+            data_type,
+            modelcard_uid,
+            feature_map,
+            sample_data_interface_type,
+            preprocessor_name,
+            metadata: metadata.unwrap_or_default(),
+        }
+    }
+}
