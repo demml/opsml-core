c4966e715ed12da99e595d5fb4c5ad66d0f58026
Thorrester <sjforrester32@gmail.com>
2024-12-19 14:09:07 -0500

    add tensorflow args

--- crates/opsml_types/src/cards/model/base.rs
+++ crates/opsml_types/src/cards/model/base.rs
@@ -393,3 +393,51 @@ impl TorchInterfaceArgs {
         }
     }
 }
+
+#[pyclass]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TensorFlowInterfaceArgs {
+    #[pyo3(get)]
+    pub task_type: String,
+    #[pyo3(get)]
+    pub model_type: String,
+    #[pyo3(get)]
+    pub data_type: String,
+    #[pyo3(get)]
+    pub modelcard_uid: String,
+    #[pyo3(get)]
+    pub feature_map: HashMap<String, Feature>,
+    #[pyo3(get)]
+    pub preprocessor_name: String,
+    #[pyo3(get)]
+    pub sample_data_interface_type: String,
+    #[pyo3(get)]
+    pub metadata: HashMap<String, String>,
+}
+
+#[pymethods]
+impl TensorFlowInterfaceArgs {
+    #[new]
+    #[pyo3(signature = (task_type, model_type, data_type, modelcard_uid, feature_map, preprocessor_name, sample_data_interface_type, metadata=None))]
+    fn new(
+        task_type: String,
+        model_type: String,
+        data_type: String,
+        modelcard_uid: String,
+        feature_map: HashMap<String, Feature>,
+        preprocessor_name: String,
+        sample_data_interface_type: String,
+        metadata: Option<HashMap<String, String>>,
+    ) -> Self {
+        TensorFlowInterfaceArgs {
+            task_type,
+            model_type,
+            data_type,
+            modelcard_uid,
+            feature_map,
+            preprocessor_name,
+            sample_data_interface_type,
+            metadata: metadata.unwrap_or_default(),
+        }
+    }
+}
